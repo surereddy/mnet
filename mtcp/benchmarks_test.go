f08516521b7afeddc20e45df41e632d8fbc7b7c7
Alexander Ewetumo <trinoxf@gmail.com>
2017-11-29 06:59:29 +0100

    Add benchmarks for message size

--- mtcp/benchmarks_test.go
+++ mtcp/benchmarks_test.go
@@ -1,6 +1,7 @@
 package mtcp_test
 
 import (
+	"math/rand"
 	"net"
 	"testing"
 	"time"
@@ -73,4 +74,127 @@ func BenchmarkNonTLSNetworkWriteWithNetConn(b *testing.B) {
 	ctx.Cancel()
 	netw.Wait()
 
+}
+
+func BenchmarkNoBytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(0)
+	benchThis(b, payload)
+}
+
+func Benchmark2BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(2)
+	benchThis(b, payload)
+}
+
+func Benchmark8BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(8)
+	benchThis(b, payload)
+}
+
+func Benchmark16BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(16)
+	benchThis(b, payload)
+}
+
+func Benchmark32BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(32)
+	benchThis(b, payload)
+}
+
+func Benchmark64BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(64)
+	benchThis(b, payload)
+}
+
+func Benchmark128BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(128)
+	benchThis(b, payload)
+}
+
+func Benchmark256BytesMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(256)
+	benchThis(b, payload)
+}
+
+func Benchmark1KMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(1024)
+	benchThis(b, payload)
+}
+
+func Benchmark4KMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(4 * 1024)
+	benchThis(b, payload)
+}
+
+func Benchmark8KMessages(b *testing.B) {
+	b.StopTimer()
+	payload := sizedPayload(8 * 1024)
+	benchThis(b, payload)
+}
+
+func benchThis(b *testing.B, payload []byte) {
+	b.StopTimer()
+
+	ctx := context.New()
+	netw, err := createNewNetwork(ctx, ":5050", nil)
+	if err != nil {
+		b.Fatalf("Failed to create network: %+q", err)
+		return
+	}
+
+	client, err := mtcp.Connect(":5050", mtcp.Metrics(events), mtcp.ClientWriteInterval(1*time.Second))
+	if err != nil {
+		b.Fatalf("Failed to dial network %+q", err)
+		return
+	}
+
+	b.StartTimer()
+	b.SetBytes(int64(len(payload)))
+	for i := 0; i < b.N; i++ {
+		client.Write(payload)
+	}
+
+	client.Flush()
+	b.StopTimer()
+	client.Close()
+	ctx.Cancel()
+	netw.Wait()
+}
+
+var pub = []byte("pub")
+var space = []byte(" ")
+var ch = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$#%^&*()")
+
+func sizedPayloadString(sz int) string {
+	return string(sizedPayload(sz))
+}
+
+func sizedPayload(sz int) []byte {
+	payload := make(sz + len(pub) + 1)
+	copy(payload, pub)
+	copy(payload, space)
+	copy(payload, sizedBytes(sz))
+	return payload
+}
+
+func sizedBytes(sz int) []byte {
+	b := make([]byte, sz)
+	for i := range b {
+		b[i] = ch[rand.Intn(len(ch))]
+	}
+	return b
+}
+
+func sizedString(sz int) string {
+	return string(sizedBytes(sz))
 }
